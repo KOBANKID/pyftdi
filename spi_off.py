906eb55625701a4c5c61bdc1a20789ff662c28ae
KOBANKID <laputa500@yahoo.co.jp>
2020-03-30 08:50:51 +0900

    Add: Add new python script

---
+++ spi_off.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+import pyftdi.spi
+import time
+
+spi = pyftdi.spi.SpiController()
+spi.configure('ftdi:///1')
+gpio = spi.get_gpio();
+gpio.set_direction(0x0100, 0x0100)
+gpio.write(0x0000)
+time.sleep(0.100)
+gpio.write(0x0000)
